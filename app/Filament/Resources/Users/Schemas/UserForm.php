b827b400793b550f26a943ce13e9dda31be69c18
Horace <horacebenjamin007@hotmail.com>
2026-06-25 01:45:25 +0100

    Associate users with clients

---
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+use Illuminate\Support\Facades\Hash;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Email address')
+                    ->email()
+                    ->required(),
+                TextInput::make('password')
+                    ->password()
+                    ->required(fn (string $operation): bool => $operation === 'create')
+                    ->dehydrateStateUsing(fn (?string $state): ?string => filled($state) ? Hash::make($state) : null)
+                    ->dehydrated(fn (?string $state): bool => filled($state)),
+                Select::make('client_id')
+                    ->relationship('client', 'company_name')
+                    ->searchable()
+                    ->preload()
+                    ->nullable(),
+            ]);
+    }
+}
